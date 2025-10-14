9adcdd4b8589ed15ace7a85d6ce8960e50f8f14d
MilivojeS2003 <milivojesindjic.s1a@gmail.com>
2025-10-14 23:56:34 +0200

    ...

---
+++ vue-project/src/views/ContactView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="contact-div">
+        <h1 class="contact-heading">OVO JE CONTACT PAGE</h1>
+    </div>
+  </main>
+</template>
+
+<style>
+@media(min-width:1024){
+
+.contact-div{
+    min-height:100vh;
+    display:flex;
+    text-align:center
+}
+
+}
+
+</style>
