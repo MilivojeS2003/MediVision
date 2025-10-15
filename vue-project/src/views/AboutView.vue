5290e3465b520a6c83a9f41179c1751e3955f346
MilivojeS2003 <milivojesindjic.s1a@gmail.com>
2025-10-15 18:00:11 +0200

    10.15.25

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,6 +1,24 @@
+<script>
+import flash from '../mixins/flash'
+
+export default{
+  mixins:[flash] //U ovu komponentu dodaj sve metode iz flash mixina
+}
+//   import swal from 'sweetalert';
+
+// export default{
+//   methods:{
+//     flash(message){
+//       swal('Success!',message, 'success')
+//     }
+//   }
+// }
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="flash('It works on the aboute page!')">Click</button>
   </div>
 </template>
 
--- vue-project/src/views/ContactView.vue
+++ vue-project/src/views/ContactView.vue
@@ -1,16 +1,40 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import {useFlash} from '@/composables/useFlash'
+const count = ref(0)
+const message = ref('Hello World')
+let { flash } = useFlash()
+
+onMounted(()=>{
+    alert('Hi there')
+})
+
+let doSomthing = () =>{
+    alert('doing it now')
+}
+
+setTimeout(()=>{
+    message.value = 'I have been changed'  //ovo je zato sto ref() ne pravi obican string vec objekat i moramo pristupiti vrijednosti value
+},2000)
 </script>
 
 <template>
   <main>
     <div class="contact-div">
         <h1 class="contact-heading">OVO JE CONTACT PAGE</h1>
+         <button @click="count++">Count is: {{ count }}</button>
+         <p>{{ message }}</p>
+         <p>
+            <input type="text" v-model="message"></input>
+         </p>
+         <button @click="doSomthing">DOING</button>
+         <button @click="flash('It works!!!')">FLASH FUNCTION</button>
     </div>
-  </main>
+</main>
 </template>
 
-<style>
+<style scoped>
 @media(min-width:1024){
 
 .contact-div{
@@ -19,6 +43,10 @@ import TheWelcome from '../components/TheWelcome.vue'
     text-align:center
 }
 
+}
+
+button {
+  font-weight: bold;
 }
 
 </style>
